d4ed8f743f165d53e0352e77032005655edb1409
Dariusz Marszałek <darekm770@gmail.com>
2022-04-02 21:24:02 +0200

    module6 UseCases tested

---
+++ app/src/test/java/dmar/akan/akademiaandroida/features/locations/domain/GetLocationsUseCaseTest.kt
@@ -0,0 +1,26 @@
+package dmar.akan.akademiaandroida.features.locations.domain
+
+import dmar.akan.akademiaandroida.features.locations.LocationRepository
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.GlobalScope
+import org.junit.jupiter.api.Test
+
+internal class GetLocationsUseCaseTest {
+
+    @Test
+    fun `when use case is invoked than execute getLocations method from repository`() {
+        //given
+        val repository = mockk<LocationRepository>(relaxed = true)
+        val useCase = GetLocationsUseCase(repository)
+
+        //when
+        useCase(
+            params = Unit,
+            scope = GlobalScope
+        )
+
+        //then
+        coVerify { repository.getLocations() }
+    }
+}
