e317d181416c15e09f21567883f9e11927353cc3
Dariusz Marszałek <darekm770@gmail.com>
2022-06-22 13:43:27 +0200

    CharacterRepositoryImplTest completed

--- app/src/test/java/dmar/akan/akademiaandroida/features/characters/data/repository/CharacterRepositoryImplTest.kt
+++ app/src/test/java/dmar/akan/akademiaandroida/features/characters/data/repository/CharacterRepositoryImplTest.kt
@@ -4,6 +4,7 @@ import dmar.akan.akademiaandroida.core.api.RickAndMortyApi
 import dmar.akan.akademiaandroida.core.api.model.CharacterResponse
 import dmar.akan.akademiaandroida.core.network.NetworkStateProvider
 import dmar.akan.akademiaandroida.features.characters.data.local.CharacterDao
+import dmar.akan.akademiaandroida.features.characters.data.local.model.CharacterCached
 import dmar.akan.akademiaandroida.features.characters.domain.CharacterRepository
 import dmar.akan.akademiaandroida.mock.mock
 import io.mockk.coEvery
@@ -33,5 +34,49 @@ internal class CharacterRepositoryImplTest {
 
         //then
         coVerify { api.getCharacter() }
+    }
+
+    @Test
+    fun `GIVEN network is connected AND successful data fetch WHEN characters request THEN save characters to local database`() {
+        //given
+        val api = mockk<RickAndMortyApi> {
+            coEvery { getCharacter() } returns CharacterResponse.mock()
+        }
+        val characterDao = mockk<CharacterDao>(relaxed = true)
+        val networkStateProvider = mockk<NetworkStateProvider> {
+            every { isNetworkAvailable() } returns true
+        }
+
+        val repository: CharacterRepository =
+            CharacterRepositoryImpl(api, characterDao, networkStateProvider)
+
+        //when
+        runBlocking { repository.getCharacters() }
+
+        //then
+        coVerify { characterDao.saveCharacters(*anyVararg()) }
+    }
+
+    @Test
+    fun `GIVEN network is disconnected WHEN characters request THEN fetch characters from local database`() {
+        //given
+        val api = mockk<RickAndMortyApi>(relaxed = true)
+
+        val characterDao = mockk<CharacterDao> {
+            coEvery { getCharacters() } returns listOf(CharacterCached.mock(), CharacterCached.mock())
+        }
+
+        val networkStateProvider = mockk<NetworkStateProvider> {
+            every { isNetworkAvailable() } returns false
+        }
+
+        val repository: CharacterRepository =
+            CharacterRepositoryImpl(api, characterDao, networkStateProvider)
+
+        //when
+        runBlocking { repository.getCharacters() }
+
+        //then
+        coVerify { characterDao.getCharacters() }
     }
 }
